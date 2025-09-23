6fa587ff4591edc4b9597d513ecaaabc6b6eb87f
JoseLeMalin <jlm_488@tutamail.com>
2025-09-23 15:04:08 +0200

    Ran prettier

--- src/WebAppReact/src/routes/customScript[.]js.ts
+++ src/WebAppReact/src/routes/customScript[.]js.ts
@@ -1,12 +1,12 @@
-import { createServerFileRoute } from '@tanstack/react-start/server'
-export const ServerRoute = createServerFileRoute('/customScript.js').methods({
+import { createServerFileRoute } from "@tanstack/react-start/server";
+export const ServerRoute = createServerFileRoute("/customScript.js").methods({
   GET: async ({ request }) => {
-    console.log('GET /customScript.js @', request.url);
-    
+    console.log("GET /customScript.js @", request.url);
+
     return new Response('console.log("Hello from customScript.js!")', {
       headers: {
-        'Content-Type': 'application/javascript',
+        "Content-Type": "application/javascript",
       },
-    })
+    });
   },
-})
+});
